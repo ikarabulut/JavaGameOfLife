7cd08839c58952601db86b17712e12dc2f3b94bd
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-04 13:53:58 -0500

    updates test to show new List abstraction and removes board.setGrid() since it is constructed when initialized

--- app/src/test/java/com/ikarabulut/BoardTest.java
+++ app/src/test/java/com/ikarabulut/BoardTest.java
@@ -1,5 +1,7 @@
 package com.ikarabulut;
+
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -12,8 +14,7 @@ class BoardTest {
     @DisplayName("Given no arguments, a 10x10 board is created by default")
     void setGridDefault() {
         Board board = new Board();
-        board.setGrid();
-        ArrayList<ArrayList> generatedBoard = board.getGrid();
+        List<ArrayList> generatedBoard = board.getGrid();
         assertAll("default grid size",
                 () -> assertEquals(10, generatedBoard.size(), "10 rows are not being generated"),
                 () -> assertEquals(10, generatedBoard.get(0).size(), "10 columns are not being generated")
@@ -24,20 +25,11 @@ class BoardTest {
     @DisplayName("Given the arguments of 20,20 are passed, a 20X20 board is created")
     void setGridCustom() {
         Board board = new Board(20,20);
-        board.setGrid();
-        ArrayList<ArrayList> generatedBoard = board.getGrid();
+        List<ArrayList> generatedBoard = board.getGrid();
         assertAll("custom grid size",
                 () -> assertEquals(20, generatedBoard.size(), "20 rows are not being generated"),
                 () -> assertEquals(20, generatedBoard.get(0).size(), "20 columns are not being generated")
         );
     }
 
-//    @Test
-//    @DisplayName("Given no arguments are passed, the grid is generated with random cells")
-//    void createRandomWorld() {
-//        Board board = new Board();
-//        board.setGrid();
-//        board.generateRandomCells();
-//
-//    }
 }
