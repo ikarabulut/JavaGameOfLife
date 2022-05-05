f18631169a81bfec36ab660646b0f407acece9af
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-05 14:24:00 -0500

    adds private methods to getCell, checkNeighbors, and public methods to make cells alive, and get count of alive neighbors

--- app/src/main/java/com/ikarabulut/Board.java
+++ app/src/main/java/com/ikarabulut/Board.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 
 public class Board {
 
+    // CONSTRUCTORS
     private int rows;
     private int columns;
     private List<ArrayList> grid;
@@ -22,6 +23,7 @@ public class Board {
         grid = setGrid();
     }
 
+    // PUBLIC METHODS
     public List<ArrayList> setGrid() {
         grid = new ArrayList<ArrayList>();
         for (int i = 0; i < columns; i++) {
@@ -49,4 +51,135 @@ public class Board {
     public List<ArrayList> getGrid() {
         return grid;
     }
+
+    public void setAliveAt(int rowNum, int colNum) {
+        Cell selectedCell = getCell(rowNum, colNum);
+        selectedCell.reviveMe();
+    }
+
+    public boolean isCellAliveAt(int rowNum, int colNum) {
+        Cell selectedCell = getCell(rowNum, colNum);
+        return selectedCell.checkIfAlive();
+    }
+
+    public int getAliveNeighborsCountOf(int rowNum,int colNum) {
+        int numOfAliveNeighbors = 0;
+        List<Cell> neighborsList = getNeighborsOf(rowNum, colNum);
+
+        for (Cell neighbor : neighborsList) {
+            if (neighbor.checkIfAlive()) {
+                numOfAliveNeighbors++;
+            }
+        }
+        return numOfAliveNeighbors;
+    }
+
+    // PRIVATE METHODS
+    private List<Cell> getNeighborsOf(int rowNum, int colNum) {
+        List<Cell> neighborsList = new ArrayList<>();
+
+        neighborsList.add(topLeftNeighbor(rowNum, colNum));
+        neighborsList.add(topNeighbor(rowNum, colNum));
+        neighborsList.add(topRightNeighbor(rowNum, colNum));
+        neighborsList.add(leftNeighbor(rowNum, colNum));
+        neighborsList.add(rightNeighbor(rowNum, colNum));
+        neighborsList.add(bottomLeftNeighbor(rowNum, colNum));
+        neighborsList.add(bottomNeighbor(rowNum, colNum));
+        neighborsList.add(bottomRightNeighbor(rowNum, colNum));
+
+        return neighborsList;
+    }
+
+    private Cell topLeftNeighbor(int rowNum, int colNum) {
+        Cell topLeftNeighbor;
+        if (rowNum == 0 && colNum == 0) {
+            topLeftNeighbor = getCell(rows - 1, columns - 1);
+        } else if (rowNum == 0) {
+            topLeftNeighbor = getCell(rows - 1,colNum - 1);
+        } else {
+            topLeftNeighbor = getCell(rowNum - 1, colNum - 1);
+        }
+        return topLeftNeighbor;
+    }
+    private Cell topNeighbor(int rowNum, int colNum) {
+        Cell topNeighbor;
+        if (rowNum == 0) {
+            topNeighbor = getCell(rows - 1, colNum);
+        } else {
+            topNeighbor = getCell(rowNum - 1, colNum);
+        }
+        return topNeighbor;
+    }
+    private Cell topRightNeighbor(int rowNum, int colNum) {
+        Cell topRightNeighbor;
+        if (rowNum == 0 && colNum == columns - 1) {
+            topRightNeighbor = getCell(rows - 1, 0);
+        } else if (colNum == columns - 1) {
+            topRightNeighbor = getCell(rowNum - 1, 0);
+        } else if (rowNum == 0) {
+            topRightNeighbor = getCell(rows - 1, colNum + 1);
+        } else {
+            topRightNeighbor = getCell(rowNum - 1, colNum + 1);
+        }
+        return topRightNeighbor;
+    }
+    private Cell leftNeighbor(int rowNum, int colNum) {
+        Cell leftNeighbor;
+        if (colNum == 0) {
+            leftNeighbor = getCell(rowNum, columns - 1);
+        } else {
+            leftNeighbor = getCell(rowNum, colNum - 1);
+        }
+        return leftNeighbor;
+    }
+    private Cell rightNeighbor(int rowNum, int colNum) {
+        Cell rightNeighbor;
+        if (colNum == columns - 1) {
+            rightNeighbor = getCell(rowNum, 0);
+        } else {
+            rightNeighbor = getCell(rowNum, colNum + 1);
+        }
+        return rightNeighbor;
+    }
+    private Cell bottomLeftNeighbor(int rowNum, int colNum) {
+        Cell bottomLeftNeighbor;
+        if (rowNum == rows - 1 && colNum == 0) {
+            bottomLeftNeighbor = getCell(0, columns - 1);
+        } else if (rowNum == rows - 1) {
+            bottomLeftNeighbor = getCell(0, colNum - 1);
+        } else if (colNum == 0) {
+            bottomLeftNeighbor = getCell(rowNum + 1, columns - 1);
+        } else {
+            bottomLeftNeighbor = getCell(rowNum + 1, colNum - 1);
+        }
+        return bottomLeftNeighbor;
+    }
+    private Cell bottomNeighbor(int rowNum, int colNum) {
+        Cell bottomNeighbor;
+        if (rowNum == rows - 1) {
+            bottomNeighbor = getCell(0, colNum);
+        } else {
+            bottomNeighbor = getCell(rowNum + 1, colNum);
+        }
+        return bottomNeighbor;
+    }
+    private Cell bottomRightNeighbor(int rowNum, int colNum) {
+        Cell bottomRightNeighbor;
+        if (rowNum == rows - 1 && colNum == columns - 1) {
+            bottomRightNeighbor = getCell(0, 0);
+        } else if (colNum == columns - 1) {
+            bottomRightNeighbor = getCell(rowNum + 1, 0);
+        } else if (rowNum == rows - 1) {
+            bottomRightNeighbor = getCell(0, colNum + 1);
+        } else {
+            bottomRightNeighbor = getCell(rowNum + 1, colNum + 1);
+        }
+        return bottomRightNeighbor;
+    }
+    private Cell getCell(int rowNum, int colNum) {
+        ArrayList<Cell> rowList = grid.get(rowNum);
+        Cell selectedCell = rowList.get(colNum);
+        return selectedCell;
+    }
+
 }
