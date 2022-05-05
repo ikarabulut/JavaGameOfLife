ca5c88b08806bf4d9092f46a31a1709f9d55bec2
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-05 16:42:31 -0500

    removes call to all jupiter.api and narrows it down to what is being used

--- app/src/test/java/com/ikarabulut/BoardTest.java
+++ app/src/test/java/com/ikarabulut/BoardTest.java
@@ -1,9 +1,12 @@
 package com.ikarabulut;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
