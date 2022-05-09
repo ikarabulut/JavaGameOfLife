d99d56f8573a6e2c21b8c2298a809f6acd1469ce
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-09 11:38:44 -0500

    updates test method names

--- app/src/test/java/com/ikarabulut/RulesTest.java
+++ app/src/test/java/com/ikarabulut/RulesTest.java
@@ -17,42 +17,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class RulesTest {
     @Test
     @DisplayName("When 0 is passed to aliveCheck, false will return, meaning cell will die")
-    void isUnderpopulated() {
+    void aliveCheck_0Passed() {
         boolean test = Rules.aliveCheck(0);
         assertFalse(test);
     }
 
     @Test
     @DisplayName("When 2 is passed to AliveCheck, True will return, meaning cell will survive")
-    void isFertileLand() {
+    void aliveCheck_2Passed() {
         boolean test = Rules.aliveCheck(2);
         assertTrue(test);
     }
 
     @Test
     @DisplayName("When 3 is passed to aliveCheck, True will return, meaning cell will survive")
-    void isFertileLand_3() {
+    void aliveCheck_3Passed() {
         boolean test = Rules.aliveCheck(3);
         assertTrue(test);
     }
 
     @Test
     @DisplayName("When 4 is passed to aliveCheck, False will return, meaning cell will die")
-    void isOverpopulated() {
+    void aliveCheck_4Passed() {
         boolean test = Rules.aliveCheck(4);
         assertFalse(test);
     }
 
     @Test
     @DisplayName("When 3 is passed to deadCheck, True will return, meaning cell will survive")
-    void isFertileLand_ForDeadCell() {
+    void deadCheck_3Passed() {
         boolean test = Rules.deadCheck(3);
         assertTrue(test);
     }
 
     @Test
     @DisplayName("When 1 is passed to deadCheck, false will return, meaning cell stays dead")
-    void deadCheck_StaysFalse() {
+    void deadCheck_1Passed() {
         boolean test = Rules.deadCheck(1);
         assertFalse(test);
     }
