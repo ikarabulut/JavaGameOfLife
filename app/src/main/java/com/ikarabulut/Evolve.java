8b846d49cc9383b3ee7e35407c04e0b15ebc1e88
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-11 14:55:38 -0500

    adds generateNextBoard()

--- app/src/main/java/com/ikarabulut/Evolve.java
+++ app/src/main/java/com/ikarabulut/Evolve.java
@@ -5,4 +5,31 @@ import java.util.List;
 
 public class Evolve {
 
+    private Board board;
+    private List<ArrayList> evolvedBoard;
+
+    public Evolve(Board board) {
+        this.board = board;
+        this.evolvedBoard = new ArrayList<>();
+    }
+
+    public Evolve() {
+        this.board = new Board();
+        this.evolvedBoard = new ArrayList<>();
+    }
+
+    public List<ArrayList> generateNextBoard(List<ArrayList> evolvingBoard) {
+        for (int row = 0; row < board.getRows(); row++) {
+            evolvedBoard.add(new ArrayList<Cell>());
+            for (int column = 0; column < board.getColumns(); column++) {
+                ArrayList<Cell> currentRow = evolvedBoard.get(row);
+
+                boolean nextGenerationStatus = Rules.determineFate(board.getNumberOfAliveNeighbors(row, column), board.isCellAliveAt(row, column));
+                currentRow.add(new Cell(nextGenerationStatus));
+            }
+        }
+
+        return evolvedBoard;
+    }
+
 }
