4055eeb3afdebc01a8b80ba651f388a53fe4b6c3
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-11 10:52:09 -0500

    creates Evolve Class with Test Suite

---
+++ app/src/test/java/com/ikarabulut/EvolveTest.java
@@ -0,0 +1,13 @@
+package com.ikarabulut;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EvolveTest {
+
+}
