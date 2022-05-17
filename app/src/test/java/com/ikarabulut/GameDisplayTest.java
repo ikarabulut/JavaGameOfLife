1c7cc41eccd6ba5dac26abf82afd83a5073a0027
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-17 13:16:29 -0500

    adds GameDisplay test

--- app/src/test/java/com/ikarabulut/GameDisplayTest.java
+++ app/src/test/java/com/ikarabulut/GameDisplayTest.java
@@ -22,4 +22,20 @@ class GameDisplayTest {
         );
     }
 
+    @Test
+    @DisplayName("When GameDisplay is initialized, all of its prompts should not be empty strings")
+    void setPrompts_NotEmpty() {
+        GameDisplay gameDisplay = new GameDisplay();
+
+        assertAll (
+                () -> assertNotEquals("", gameDisplay.welcomePrompt()),
+                () -> assertNotEquals("", gameDisplay.gameSettingsPrompt()),
+                () -> assertNotEquals("", gameDisplay.evolutionsPrompt()),
+                () -> assertNotEquals("", gameDisplay.rowsPrompt()),
+                () -> assertNotEquals("", gameDisplay.columnsPrompt()),
+                () -> assertNotEquals("", gameDisplay.aliveSymbolPrompt()),
+                () -> assertNotEquals("", gameDisplay.deadSymbolPrompt())
+        );
+    }
+
 }
