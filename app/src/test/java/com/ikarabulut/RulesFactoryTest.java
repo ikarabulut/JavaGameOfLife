bb97c2f3976f7218e9005182e327d58824fccc09
Ismail Karabulut <i.karabulut93@gmail.com>
2022-05-18 12:09:04 -0500

    adds GameFactory tests

---
+++ app/src/test/java/com/ikarabulut/RulesFactoryTest.java
@@ -0,0 +1,30 @@
+package com.ikarabulut;
+
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RulesFactoryTest {
+
+    @Test
+    @DisplayName("When 1 is passed in to getRules, then the return will be a new Rules class object")
+    void getRules_pass1() {
+        RulesFactory factory = new RulesFactory();
+        RuleSet returnValue = factory.getRules(1);
+
+        MatcherAssert.assertThat(returnValue, instanceOf(Rules.class));
+    }
+
+    @Test
+    @DisplayName("When 2 is passed in to getRules, then the return will be a new Rules1 class object")
+    void getRules_pass2() {
+        RulesFactory factory = new RulesFactory();
+        RuleSet returnValue = factory.getRules(2);
+
+        MatcherAssert.assertThat(returnValue, instanceOf(Rules1.class));
+    }
+
+}
